03fa44b66aa0fff1fae17aaf948963cbe01a0cd6
oviedo2522 <oviedo2522@gmail.com>
2024-09-14 10:23:42 -0700

    New Files

---
+++ build/utility.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = add;
+// adds 2 numbers                                          
+function add(x, y) {
+    return x + y;
+}
